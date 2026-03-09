c32532335dedd01a887ad1921394aa01cf23dceb
IonicDesertion <mr.daniel.8.1.17@gmail.com>
2026-03-09 16:28:17 -0400

    cambios antes de limpiar historial

--- Utilidades/Alumno.php
+++ Utilidades/Alumno.php
@@ -2,18 +2,57 @@
 
 namespace Utilidades;
 
-class Alumno 
+class Alumno
 {
     public function __construct(
-    public string $cedula,
-    public string $nombre,
-    public string $apellido,
-    public int $edad,
-    public float $altura,
-    public float $matricula,
-    public bool $sexo, // true = femenino, false = masculino
-    public int $turno, // 1 = mañana, 2 = tarde, 3 = noche
-    ){} 
-    
-    
+        public string $cedula,
+        public string $nombre,
+        public string $apellido,
+        public int $edad,
+        public float $altura,
+        public float $matricula,
+        public bool $sexo, // true = femenino, false = masculino
+        public int $turno, // 1 = mañana, 2 = tarde, 3 = noche
+    ) {}
+
+    // Getters
+    public function getCedula(): string
+    {
+        return $this->cedula;
+    }
+
+    public function getNombre(): string
+    {
+        return $this->nombre;
+    }
+
+    public function getApellido(): string
+    {
+        return $this->apellido;
+    }
+
+    public function getEdad(): int
+    {
+        return $this->edad;
+    }
+
+    public function getAltura(): float
+    {
+        return $this->altura;
+    }
+
+    public function getMatricula(): float
+    {
+        return $this->matricula;
+    }
+
+    public function isSexo(): bool
+    {
+        return $this->sexo;
+    }
+
+    public function getTurno(): int
+    {
+        return $this->turno;
+    }
 }
--- registro.php
+++ registro.php
@@ -5,10 +5,21 @@ declare(strict_types=1);
 require_once 'Utilidades/Utilidades.php';
 
 use Utilidades\Utilidades as Util;
+use Utilidades\Alumno as Alumno;
 
 function main(): void
 {
     Util::encabezado('Registro de Alumnos', 45);
+    Alumno $alumno = new Alumno(
+        '26.144.789',
+        'Maritza',
+        'lopez',
+        23,
+        1.74,
+        1256.66,
+        true,
+        1
+    );
     $nombre = Util::leerTexto('Ingresa tu nombre');
     $apellido = Util::leerTexto('Ingresa tu apellido');
     $email = Util::leerTexto('Ingresa tu correo electrónico');
